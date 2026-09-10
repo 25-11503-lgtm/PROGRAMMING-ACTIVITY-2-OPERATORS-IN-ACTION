8c70bc36190c9b9d86789deac4789af3ab5850f7
25-11503-lgtm <25-11503@g.batstate-u.edu.ph>
2026-09-10 14:08:39 +0800

    Add parity check for user-input integer

---
+++ act2_parity.py
@@ -0,0 +1,11 @@
+name = input("Enter your name")
+print("Hello," + name)
+
+# act2_parity.py
+# TASK 2 - EVEN OR ODD
+
+# Read integer from user
+num = int(input("Enter an interger: "))
+
+# Check parity using % and == only - no if statement
+print(num % 2==0)     
